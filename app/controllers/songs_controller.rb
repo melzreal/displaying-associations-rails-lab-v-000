2d0b0ee5aa3290ef58eb383cbb12c5063b89bc59
Melissa Real <melissa.real@hotmail.com>
2019-01-12 18:48:59 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,10 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
@@ -41,10 +43,11 @@ class SongsController < ApplicationController
     redirect_to songs_path
   end
 
+
+
   private
 
   def song_params
     params.require(:song).permit(:title)
   end
 end
-
